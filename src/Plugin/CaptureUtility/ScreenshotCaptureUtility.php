f07cee9c040248672e0c1c3a0ea43b90ea7e4fdc
davi4797 <david.porter@rackspace.com>
2017-07-22 00:00:21 -0500

    Issue #2894732 by bighappyface, WidgetsBurritos: Add Screenshot Options to Config Entity Setting

--- src/Plugin/CaptureUtility/ScreenshotCaptureUtility.php
+++ src/Plugin/CaptureUtility/ScreenshotCaptureUtility.php
@@ -95,6 +95,34 @@ class ScreenshotCaptureUtility extends ConfigurableCaptureUtilityBase {
       '#description' => $this->t('Specify the width you would like to capture.'),
       '#default_value' => 1280,
     ];
+    $form['clip_width'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Capture clip width (in pixels)'),
+      '#description' => $this->t('Specify the clip width you would like to capture.'),
+      '#default_value' => 1280,
+    ];
+    $form['background_color'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Browser background color'),
+      '#description' => $this->t('Specify the browser background color. Please use a hex color value.'),
+      '#default_value' => '#ffffff',
+    ];
+    $form['user_agent'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Browser user agent'),
+      '#description' => $this->t('Specify the browser user agent. e.g. "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"'),
+      '#default_value' => '',
+    ];
+    $image_types = \Screen\Image\Types::available();
+    $image_types = array_combine($image_types, $image_types);
+    $form['image_type'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Image type'),
+      '#options' => $image_types,
+      '#empty_option' => $this->t('Select an image type'),
+    ];
+
+    // Screen\Image\Types
     return $form;
   }
 
@@ -105,6 +133,10 @@ class ScreenshotCaptureUtility extends ConfigurableCaptureUtilityBase {
     parent::submitConfigurationForm($form, $form_state);
 
     $this->configuration['width'] = $form_state->getValue('width');
+    $this->configuration['clip_width'] = $form_state->getValue('clip_width');
+    $this->configuration['background_color'] = $form_state->getValue('background_color');
+    $this->configuration['user_agent'] = $form_state->getValue('user_agent');
+    $this->configuration['image_type'] = $form_state->getValue('image_type');
   }
 
 }
