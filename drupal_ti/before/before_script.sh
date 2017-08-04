ad91a70eb053158e4d9aec2537757848673cdc6b
WidgetsBurritos <davidstinemetze@gmail.com>
2017-08-03 23:59:52 -0500

    test(cron): Add cron unit tests

---
+++ drupal_ti/before/before_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Add an optional statement to see that this is running in Travis CI.
+echo "running drupal_ti/before/before_script.sh"
+
+set -e $DRUPAL_TI_DEBUG
+
+# Ensure the right Drupal version is installed.
+# The first time this is run, it will install Drupal.
+# Note: This function is re-entrant.
+drupal_ti_ensure_drupal
+
+# Change to the Drupal directory
+cd "$DRUPAL_TI_DRUPAL_DIR"
+
+composer require "mtdowling/cron-expression:1.2.0"
---
+++ tests/src/Unit/Cron/CronRunnerTest.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Drupal\Tests\web_page_archive\Unit\Cron;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\web_page_archive\Cron\CronRunner;
+
+/**
+ * @coversDefaultClass \Drupal\web_page_archive\Cron\CronRunner
+ *
+ * @group web_page_archive
+ */
+class CronRunnerTest extends UnitTestCase {
+
+  /**
+   * Helper function to get a mock web page archive with pre-defined crontab.
+   */
+  private function getMockWebPageArchive($crontab) {
+    $mock_web_page_archive = $this->getMockBuilder('\Drupal\web_page_archive\Entity\WebPageArchive')
+      ->disableOriginalConstructor()
+      ->getMock();
+    $mock_web_page_archive->expects($this->any())
+      ->method('id')
+      ->will($this->returnValue('abc123'));
+    $mock_web_page_archive->expects($this->any())
+      ->method('getCronSchedule')
+      ->will($this->returnValue($crontab));
+
+    return $mock_web_page_archive;
+  }
+
+  /**
+   * Helper function to get a mock time service.
+   */
+  private function getMockTime($timestamp = '1501774086') {
+    $mock_time = $this->getMockBuilder('\Drupal\Component\Datetime\TimeInterface')
+      ->getMock();
+    $mock_time->expects($this->any())
+      ->method('getRequestTime')
+      ->will($this->returnValue($timestamp));
+
+    return $mock_time;
+  }
+
+  /**
+   * Helper function to get a mock lock service.
+   */
+  private function getMockLock($acquire = TRUE) {
+    // Setup mock lock service.
+    $mock_lock = $this->getMockBuilder('\Drupal\Core\Lock\LockBackendInterface')
+      ->getMock();
+    $mock_lock->expects($this->any())
+      ->method('acquire')
+      ->will($this->returnValue($acquire));
+    $mock_lock->expects($this->any())
+      ->method('release');
+
+    return $mock_lock;
+  }
+
+  /**
+   * Helper function to get a mock state service.
+   */
+  private function getMockState() {
+    $mock_state = $this->getMockBuilder('\Drupal\Core\State\StateInterface')
+      ->getMock();
+
+    return $mock_state;
+  }
+
+  /**
+   * Helper function to get a cron runner based on default or supplied mocks.
+   */
+  private function getCronRunner(array $options = []) {
+    if (empty($options['mock_time'])) {
+      $options['mock_time'] = $this->getMockTime();
+    }
+    if (empty($options['mock_lock'])) {
+      $options['mock_lock'] = $this->getMockLock();
+    }
+    if (empty($options['mock_state'])) {
+      $options['mock_state'] = $this->getMockState();
+    }
+
+    return new CronRunner($options['mock_lock'], $options['mock_state'], $options['mock_time']);
+  }
+
+  /**
+   * Assures new entities, run when it is time.
+   */
+  public function testNewEntitiesRunWhenItIsTime() {
+    $mock_state = $this->getMockState();
+    $mock_state->expects($this->once())
+      ->method('get')
+      ->will($this->returnValue(-1));
+
+    // Setup web page archive responses.
+    $mock_web_page_archive = $this->getMockWebPageArchive('28 1 * * *');
+    $mock_web_page_archive->expects($this->once())
+      ->method('startNewRun')
+      ->will($this->returnValue(TRUE));
+
+    $cron_runner = $this->getCronRunner(['mock_state' => $mock_state]);
+    $this->assertTrue($cron_runner->run($mock_web_page_archive));
+  }
+
+  /**
+   * Assures new entities, not inside the scheduled cron window don't run.
+   */
+  public function testNewEntitiesDontRunUntilTime() {
+    $mock_state = $this->getMockState();
+    $mock_state->expects($this->once())
+      ->method('get')
+      ->will($this->returnValue(-1));
+
+    // Setup web page archive responses.
+    $cron_runner = $this->getCronRunner(['mock_state' => $mock_state]);
+    $mock_web_page_archive = $this->getMockWebPageArchive('30 * * * *');
+    $this->assertFalse($cron_runner->run($mock_web_page_archive));
+  }
+
+  /**
+   * Tests that a cron won't run twice in the same minute.
+   */
+  public function testCronTabOnlyRunsOneTimePerMinute() {
+    // Setup cron runner.
+    // Important: at() is index based on all method calls on the mock, not just
+    // the specified method. That means, that ->set() calls count against this
+    // index. This could potentially cause volatility in these tests in the
+    // future. We should look into a more steady solution in the future.
+    $mock_state = $this->getMockState();
+    $mock_state->expects($this->at(0))
+      ->method('get')
+      ->will($this->returnValue('0'));
+    $mock_state->expects($this->at(2))
+      ->method('get')
+      ->will($this->returnValue('1501774146'));
+
+    $cron_runner = $this->getCronRunner(['mock_state' => $mock_state]);
+
+    $mock_web_page_archive = $this->getMockWebPageArchive('* * * * *');
+
+    // Affirm run() only completes once.
+    $mock_web_page_archive->expects($this->once())
+      ->method('startNewRun')
+      ->will($this->returnValue(TRUE));
+    $this->assertTrue($cron_runner->run($mock_web_page_archive));
+    $this->assertFalse($cron_runner->run($mock_web_page_archive));
+  }
+
+  /**
+   * Tests invalid crontab throws an exception.
+   *
+   * @expectedException Exception
+   * @expectedExceptionMessage Invalid crontab expression
+   */
+  public function testInvalidCronTabThrowsException() {
+    $mock_web_page_archive = $this->getMockWebPageArchive('I am a bad bad crontab.');
+    $cron_runner = $this->getCronRunner();
+    $cron_runner->run($mock_web_page_archive);
+  }
+
+  /**
+   * Tests lock failure prevents run.
+   */
+  public function testLockAcquireFailurePreventsRun() {
+    $mock_web_page_archive = $this->getMockWebPageArchive('* * * * *');
+    $mock_lock = $this->getMockLock(FALSE);
+    $cron_runner = $this->getCronRunner(['mock_lock' => $mock_lock]);
+    $this->assertFalse($cron_runner->run($mock_web_page_archive));
+  }
+
+}
