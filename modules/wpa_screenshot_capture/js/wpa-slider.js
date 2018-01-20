8c17423ce01366c87778af9a9e12517e1ead65fe
WidgetsBurritos <davidstinemetze@gmail.com>
2018-01-20 14:14:14 -0600

    Issue #2937233 by WidgetsBurritos: Screenshot Before/After Redraw Issue

--- modules/wpa_screenshot_capture/js/wpa-slider.js
+++ modules/wpa_screenshot_capture/js/wpa-slider.js
@@ -1,32 +1,40 @@
-/**
- * Initializes the WPA comparison slider.
- */
-function wpaInitializeSlider() {
-  var images = document.querySelectorAll('.image-style-web-page-archive-full');
-  var ct = 0;
+/** global: jQuery */
+/** global: Drupal */
 
-  /**
-   * Callback function when image is loaded.
-   */
-  function imageReady() {
-    if (++ct >= 2) {
-      jQuery('.ba-slider').beforeAfter();
-    }
-  }
+(function ($, Drupal) {
+  Drupal.behaviors.wpaBeforeAfterSlider = {
+    attach: function (context, settings) {
+      var $modal = $('#drupal-modal');
+      var images = document.querySelectorAll('.image-style-web-page-archive-full');
 
-  /**
-   * Loops through all the images to ensure they load properly.
-   */
-  for (i=0; i<images.length; i++) {
-    var image = images[i];
-    if (image.complete) {
-      imageReady();
-    }
-    else {
-      image.addEventListener('load', imageReady);
-      image.addEventListener('error', function() {
-        alert(Drupal.t('Could not load image.'));
+      // When the modal resizes, we need to ensure the images are no wider than
+      // the modal itself, otherwise before/after will scale the resize
+      // image differently.
+      $modal.on('dialogContentResize', function() {
+        var maxWidth = $(this).width();
+        for (var i=0; i<images.length; i++) {
+          images[i].style.maxWidth = maxWidth + 'px';
+        }
+      })
+
+      // Ensure both images load properly and then trigger before/after.
+      $modal.once('wpaBeforeAfterSlider').each(function () {
+        var imageLoadedCt = 0;
+        for (var i=0; i<images.length; i++) {
+          // Only trigger before/after once both images have loaded.
+          images[i].addEventListener('load', function() {
+            if (++imageLoadedCt == 2) {
+              $('.wpa-slider').beforeAfter();
+            }
+          });
+
+          // If either image fails to load for now we just open an alert.
+          // We may want to find a better solution for this in the future.
+          images[i].addEventListener('error', function() {
+            alert(Drupal.t('Could not load image.'));
+          });
+        }
       });
     }
-  }
-}
+  };
+})(jQuery, Drupal);
